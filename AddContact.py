7271ccd5eeb8b6c7450c41553303878904923b57
Jessica Batta <56795988+battajes@users.noreply.github.com>
2022-01-22 23:27:41 -0500

    Update AddContact.py

--- AddContact.py
+++ AddContact.py
@@ -1,9 +1,5 @@
 
 
-=== Module description ===
-This module contains two sample classes Tweet and User that we developed
-as way to introd
-uce the major concepts of object-oriented programming.
 """
 # Allows forward references in type annotations.
 from __future__ import annotations
